7720035cbcf29ae0c9f39a9e853ba53e5fd5fafb
Santiago Palladino <spalladino@gmail.com>
2020-05-06 20:36:21 -0300

    Connect frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useRef, useState, useEffect } from 'react';
+import { withAuthenticator } from '@aws-amplify/ui-react'
+import { createRelayer, Relayer, getRelayers } from './api/relayer';
 import './App.css';
 
 function App() {
+  const [relayers, setRelayers] = useState<Relayer[]>([])
+  const updateRelayers = () => { getRelayers().then(rs => setRelayers(rs)) };
+  useEffect(updateRelayers, []);
+
+  const form = useRef<HTMLFormElement>(null);
+  const handleSubmit = async () => {
+    if (form.current) {
+      const { relayerName } = form.current;
+      await createRelayer(relayerName.value);
+      form.current.reset();
+      updateRelayers();
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Create a relayer</h1>
+        <form ref={form}>
+          <label>Name: </label>
+          <input name="relayerName" />
+        </form>
+        <button type="button" onClick={handleSubmit}>Create</button>
+      </div>
+      <hr />
+      <div>
+        <h1>Relayers</h1>
+        { relayers.map((relayer) => (
+          <div>{relayer.name} {relayer.relayerId}</div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
